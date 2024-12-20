9e8b432dc11b9f04a13c24c5f797ab126ce10943
shresthanikhil16 <shresthanikhil16@gmail.com>
2024-12-20 19:12:39 +0545

    my card view changes

--- lib/core/common/my_card_view.dart
+++ lib/core/common/my_card_view.dart
@@ -4,50 +4,71 @@ class MyCardView extends StatelessWidget {
   final String label;
   final Color color;
   final IconData icon;
+  final bool isGradient;
+  final Widget page;
 
-  const MyCardView(
-      {super.key,
-      required this.label,
-      required this.color,
-      required this.icon});
+  const MyCardView({
+    super.key,
+    required this.label,
+    required this.color,
+    required this.icon,
+    this.isGradient = false,
+    required this.page,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            offset: const Offset(0, 5),
-            color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 2,
-            blurRadius: 5,
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Circle with solid color background
-          Container(
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: color,
-              shape: BoxShape.circle,
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the respective page
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => page),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(0, 5),
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
             ),
-            child: Icon(icon, color: Colors.white, size: 28),
-          ),
-          const SizedBox(height: 8),
-          // Label
-          Text(
-            label.toUpperCase(),
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
-                ),
-          ),
-        ],
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Circle with solid color or gradient background
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: isGradient
+                    ? const LinearGradient(
+                        colors: [Colors.redAccent, Colors.deepOrange],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      )
+                    : null,
+                color: isGradient ? null : color,
+              ),
+              child: Icon(icon, color: Colors.white, size: 28),
+            ),
+            const SizedBox(height: 8),
+            // Label
+            Text(
+              label.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+          ],
+        ),
       ),
     );
   }
