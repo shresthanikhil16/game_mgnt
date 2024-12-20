b85beeac220b3fcd1c313c53d1c95f1bbbcf51d5
shresthanikhil16 <shresthanikhil16@gmail.com>
2024-12-20 19:11:58 +0545

    main changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:game_mgnt/core/app_theme/app_theme.dart';
-import 'package:game_mgnt/view/get_started_view.dart';
+import 'package:game_mgnt/view/app.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      theme: getApplicationTheme(),
-      routes: {
-        '/': (context) => const GetStartedView(),
-        '/output': (context) => const GetStartedView(),
-      },
-    ),
-  );
+  runApp(const MyApp());
 }
