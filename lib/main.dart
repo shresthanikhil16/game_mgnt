a4028d04d7de8438f5d4371daad17a0bc1cf9b60
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-01-17 20:56:07 +0545

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
-import 'package:flutter/material.dart';
-import 'package:game_mgnt/view/app.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:game_mgnt/features/auth/presentation/view_model/login/login_bloc.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'app/app.dart';
+import 'app/di/di.dart';
+import 'core/network/hive_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive Database
+  await HiveService.init();
+
+  // Initialize Dependencies
+  await initDependencies();
+
+  // Check if LoginBloc is registered
+  print("Is LoginBloc registered: ${getIt.isRegistered<LoginBloc>()}");
+
+  // Run the app
+  runApp(App());
 }
