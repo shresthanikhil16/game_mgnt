2ee8c619c99337b4a3ae25d6dd041a94a357a1f9
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-01-30 00:17:17 +0545

    part g

--- lib/features/auth/domain/entity/auth_entity.dart
+++ lib/features/auth/domain/entity/auth_entity.dart
@@ -4,6 +4,7 @@ class AuthEntity extends Equatable {
   final String? userId;
   final String username; // updated field
   final String email; // updated field
+  final String? image;
   final String password; // updated field
   final String confirmPassword; // updated field
 
@@ -11,11 +12,12 @@ class AuthEntity extends Equatable {
     this.userId,
     required this.username, // updated field
     required this.email, // updated field
+    this.image,
     required this.password, // updated field
     required this.confirmPassword, // updated field
   });
 
   @override
   List<Object?> get props =>
-      [userId, username, email, password, confirmPassword];
+      [userId, username, email, image, password, confirmPassword];
 }
