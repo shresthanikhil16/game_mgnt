299dd8b20aca2b47f698a8b1ab7ea325a3101089
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-01-30 23:49:23 +0545

    remote local

--- lib/features/auth/data/repository/auth_remote_repository/auth_remote_repository.dart
+++ lib/features/auth/data/repository/auth_remote_repository/auth_remote_repository.dart
@@ -1,11 +1,27 @@
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
-import 'package:game_mgnt/core/error/failure.dart';
-import 'package:game_mgnt/features/auth/domain/entity/auth_entity.dart';
-import 'package:game_mgnt/features/auth/domain/repository/auth_repository.dart';
+
+import '../../../../../core/error/failure.dart';
+import '../../../domain/entity/auth_entity.dart';
+import '../../../domain/repository/auth_repository.dart';
+import '../../data_source/auth_remote_data_source/auth_remote_data_source.dart';
 
 class AuthRemoteRepository implements IAuthRepository {
+  final AuthRemoteDataSource _authRemoteDataSource;
+
+  AuthRemoteRepository(this._authRemoteDataSource);
+
+  @override
+  Future<Either<Failure, void>> registerStudent(AuthEntity student) async {
+    try {
+      await _authRemoteDataSource.registerStudent(student);
+      return Right(null);
+    } catch (e) {
+      return Left(ApiFailure(message: e.toString()));
+    }
+  }
+
   @override
   Future<Either<Failure, AuthEntity>> getCurrentUser() {
     // TODO: implement getCurrentUser
@@ -14,24 +30,22 @@ class AuthRemoteRepository implements IAuthRepository {
 
   @override
   Future<Either<Failure, String>> loginStudent(
-      String username, String password) {
-    // TODO: implement loginStudent
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, void>> registerStudent(AuthEntity student) async {
+      String email, String password) async {
     try {
-      await _authRemoteDataSource.registerStudent(student);
-      return right(null);
+      final token = await _authRemoteDataSource.loginStudent(email, password);
+      return Right(token);
     } catch (e) {
       return Left(ApiFailure(message: e.toString()));
     }
   }
 
   @override
-  Future<Either<Failure, String>> uploadProfilePicture(File file) {
-    // TODO: implement uploadProfilePicture
-    throw UnimplementedError();
+  Future<Either<Failure, String>> uploadProfilePicture(File file) async {
+    try {
+      final imageName = await _authRemoteDataSource.uploadProfilePicture(file);
+      return Right(imageName);
+    } catch (e) {
+      return Left(ApiFailure(message: e.toString()));
+    }
   }
 }
