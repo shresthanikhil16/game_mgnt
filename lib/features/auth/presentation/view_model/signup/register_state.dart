e81108744603b15acf144278efd2722935d048c9
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-01-30 23:50:56 +0545

    sate

--- lib/features/auth/presentation/view_model/signup/register_state.dart
+++ lib/features/auth/presentation/view_model/signup/register_state.dart
@@ -3,23 +3,32 @@ part of 'register_bloc.dart';
 class RegisterState {
   final bool isLoading;
   final bool isSuccess;
+  final String? imageName;
 
   RegisterState({
     required this.isLoading,
     required this.isSuccess,
+    this.imageName,
   });
 
+  // Initial state constructor
   RegisterState.initial()
       : isLoading = false,
-        isSuccess = false;
+        isSuccess = false,
+        imageName = null;
 
+  // CopyWith method to update state
   RegisterState copyWith({
     bool? isLoading,
     bool? isSuccess,
+    String? imageName, // Add imageName here to allow updating it
   }) {
     return RegisterState(
       isLoading: isLoading ?? this.isLoading,
       isSuccess: isSuccess ?? this.isSuccess,
+      imageName: imageName ?? this.imageName, // Ensure imageName can be updated
     );
   }
+
+  List<Object?> get props => [isLoading, isSuccess, imageName];
 }
