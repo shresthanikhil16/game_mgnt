8d8077a890cdda372f5d6a87b77caea8e1b8f8e9
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-01-30 23:48:33 +0545

    token

--- lib/app/shared_prefs/token_shared_prefs.dart
+++ lib/app/shared_prefs/token_shared_prefs.dart
@@ -1,1 +1,27 @@
-class TokenSharedPrefs {}
+import 'package:dartz/dartz.dart';
+import 'package:game_mgnt/core/error/failure.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TokenSharedPrefs {
+  final SharedPreferences _sharedPreferences;
+
+  TokenSharedPrefs(this._sharedPreferences);
+
+  Future<Either<Failure, void>> saveToken(String token) async {
+    try {
+      await _sharedPreferences.setString('token', token);
+      return Right(null);
+    } catch (e) {
+      return Left(SharedPrefsFailure(message: e.toString()));
+    }
+  }
+
+  Future<Either<Failure, String>> getToken() async {
+    try {
+      final token = _sharedPreferences.getString('token');
+      return Right(token ?? '');
+    } catch (e) {
+      return Left(SharedPrefsFailure(message: e.toString()));
+    }
+  }
+}
