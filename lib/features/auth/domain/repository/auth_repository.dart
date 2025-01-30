e69a8899e1547f55c4ea4482d9b934346211c19f
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-01-30 23:49:32 +0545

    auth repo

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -8,8 +8,7 @@ import '../entity/auth_entity.dart';
 abstract interface class IAuthRepository {
   Future<Either<Failure, void>> registerStudent(AuthEntity student);
 
-  Future<Either<Failure, String>> loginStudent(
-      String username, String password);
+  Future<Either<Failure, String>> loginStudent(String email, String password);
 
   Future<Either<Failure, String>> uploadProfilePicture(File file);
 
