f8f904c4bdbd45d790bcc82af09a9c8020279deb
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-01-30 23:49:06 +0545

    remote

--- lib/features/auth/data/data_source/auth_remote_data_source/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/auth_remote_data_source/auth_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:game_mgnt/app/constants/api_endpoint.dart';
 import 'package:game_mgnt/features/auth/data/data_source/auth_data_source.dart';
@@ -14,12 +16,12 @@ class AuthRemoteDataSource implements IAuthDataSource {
   }
 
   @override
-  Future<String> loginStudent(String username, String password) async {
+  Future<String> loginStudent(String email, String password) async {
     try {
       Response response = await _dio.post(
         ApiEndpoints.login,
         data: {
-          "username": username,
+          "email": email,
           "password": password,
         },
       );
@@ -49,6 +51,39 @@ class AuthRemoteDataSource implements IAuthDataSource {
       });
       if (response.statusCode == 201) {
         return;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } on DioException catch (e) {
+      throw Exception(e);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<String> uploadProfilePicture(File file) async {
+    try {
+      String fileName = file.path.split('/').last;
+      FormData formData = FormData.fromMap(
+        {
+          'profilePicture': await MultipartFile.fromFile(
+            file.path,
+            filename: fileName,
+          ),
+        },
+      );
+
+      Response response = await _dio.post(
+        ApiEndpoints.uploadImage,
+        data: formData,
+      );
+
+      if (response.statusCode == 200) {
+        // Extract the image name from the response
+        final str = response.data['data'];
+
+        return str;
       } else {
         throw Exception(response.statusMessage);
       }
