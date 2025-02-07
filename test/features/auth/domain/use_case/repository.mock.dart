c40e67d2067ad25bf5b8dd1fd69f7d39399a6983
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-02-07 10:39:09 +0545

    added

---
+++ test/features/auth/domain/use_case/repository.mock.dart
@@ -0,0 +1,4 @@
+import 'package:game_mgnt/features/auth/domain/repository/auth_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepository extends Mock implements IAuthRepository {}
---
+++ test/features/auth/domain/use_case/token.mock.dart
@@ -0,0 +1,4 @@
+import 'package:game_mgnt/app/shared_prefs/token_shared_prefs.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockTokenSharedPrefs extends Mock implements TokenSharedPrefs {}
