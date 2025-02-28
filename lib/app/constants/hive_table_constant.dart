0f1c12739c984bc317541c409bf45875d719fed9
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-02-28 19:50:25 +0545

    added

--- lib/app/constants/hive_table_constant.dart
+++ lib/app/constants/hive_table_constant.dart
@@ -3,4 +3,7 @@ class HiveTableConstant {
 
   static const int studentTableId = 0;
   static const String studentBox = 'studentBox';
+
+  static const int tournamentTableId = 1;
+  static const String tournamentBox = 'tournamentBox';
 }
